e58cbd08bd5efde8b71c30d4fe29c13fca1cac71
dogwaffles <ldreillywillhight@students.everettcc.edu>
2025-06-01 20:24:26 -0700

    creating a deckstack done in classDeal

--- class/ClassDeck.py
+++ class/ClassDeck.py
@@ -5,20 +5,14 @@ from ClassCard import Card
 #DECKNUM
 #accepts a number of decks
 #returns a number of cards in that many decks (decks * 52)
-def deckNum(decks):
-    return (decks * 52)
 
 # general variables
 
-aceVal = 1 #placeHolder
-cardVals = [aceVal,2,3,4,5,6,7,8,9,10,10,10,10]
-cardFaces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-cardSuits = ["spades", "hearts", "diamonds", "clubs"]
 
 #DEL    
-deckLength = 1
-deckLength = deckNum(deckLength)
-print(deckLength)
+# deckLength = 1
+# deckLength = deckNum(deckLength)
+# print(deckLength)
 
 
 #try_2
@@ -26,31 +20,33 @@ class Deck:
     def __init__(deck, deckLength):
         deck.deckLength = deckLength
     
+    def deckNum(decks):
+        return (decks * 52)
 
     # def deckMake(deck):
     #     print(list(range(1, deck.deckLength + 1)))
     def deckMake():
         #aceVal is placeholder
-        aceVal = 1
-        newDeck = []
+        aceVal = 1 #placeHolder
         cardVals = [aceVal,2,3,4,5,6,7,8,9,10,10,10,10]
         cardFaces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         cardSuits = ["spades", "hearts", "diamonds", "clubs"]
+        newDeck = []
         for suitsIndex in range(len(cardSuits)):
             for facesIndex in range(len(cardFaces)):
                 newDeck.append([cardVals[facesIndex], cardFaces[facesIndex], cardSuits[suitsIndex]])
         return newDeck
         # print(list(newDeck))
-        #         newCard = [cardVals[facesIndex], (Card.cardFaces[facesIndex]), (Card.cardSuits[suitsIndex])]
-        #         print(newCard)
-        #         newDeck.append(newCard)
-        #         print(newDeck)
+            #         newCard = [cardVals[facesIndex], (Card.cardFaces[facesIndex]), (Card.cardSuits[suitsIndex])]
+            #         print(newCard)
+            #         newDeck.append(newCard)
+            #         print(newDeck)
 
 
 
 # newDeck = Deck(deckLength)
     
-    print(deckMake())
+    # print(deckMake())
     
 
 # # from cardClass (WORKS)
